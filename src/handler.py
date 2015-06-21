bd23b49ba00bc1e27bc520b89b61cd08e775c8ea
Sergey Voronov <rdkl.hrd@gmail.com>
2015-06-22 01:58:02 +0300

    Added handler

---
+++ src/handler.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from src.document_storage import DocumentStorage
+from src.file_reader import FileReader
+
+##############################################################################
+class Handler(object):
+    def __init__(self):
+        self.__storage = DocumentStorage()
+        self.__storage.Load()
+        
+        self.__reader = FileReader('../data/small_ethnic_data_lem.csv',
+                                   '../data/small_ethnic_data_no_lem.csv')
+        self.__gen = self.__reader.GetTextGenerator()
+        self.__last_text_item = None
+        
+    #-------------------------------------------------------------------------
+    def GetText(self):
+        self.__last_text_item = self.__gen.next()
+        return self.__last_text_item
+
+    #-------------------------------------------------------------------------
+    def SetState(self, state):
+        self.__last_text_item.set_state(state)
+        self.__storage.AddDocument(self.__last_text_item.id, 
+                                   self.__last_text_item)
+            
+    #-------------------------------------------------------------------------
+    def __del__(self):
+        self.__storage.Save()
+
+    #-------------------------------------------------------------------------            
+##############################################################################
+
+if __name__ == "__main__":
+    file_reader = Handler()
+    print file_reader.GetText()
