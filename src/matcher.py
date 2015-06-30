f2876a2541085d8c3066106e14fec726a253a745
Sergey Voronov <rdkl.hrd@gmail.com>
2015-06-30 16:40:18 +0300

    Added matcher wrapper

---
+++ src/matcher.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import src.libs.AhoCorasick as AhoCorasick
+
+##############################################################################
+class Matcher(object):
+    """ 
+    A wrapper for Aho-Corasick from libs.
+    """
+    def __init__(self, filename="../data/ethnic_words.txt"):
+        self.__matcher = AhoCorasick.Matcher()
+        self.__matcher.Init(filename)
+        self.__dict = {}
+        self.__ReadWords(filename)
+        
+    #-------------------------------------------------------------------------    
+    def __ReadWords(self, filename):
+        with open(filename, "r") as f:
+            word_number = 0
+            for line in f.readlines():
+                # First line has length ~5.
+                if len(line) > 5:
+                    self.__dict[word_number] = line[:-1]
+                    word_number += 1
+                    
+    #-------------------------------------------------------------------------    
+    def FindWordsInText(self, text):
+        return self.__matcher.FindWordsInText(" " + text)
+    
+    #-------------------------------------------------------------------------
+    def GetDict(self):
+        return self.__dict
+    
+    #-------------------------------------------------------------------------
+##############################################################################
+
+if __name__ == "__main__":
+    m = Matcher()
+    a =  m.FindWordsInText("абориген полуюжноафриканцы тат")
+    dict = m.GetDict()
+    print len("абориген"), len(dict[0])
+    for item in a:
+        print dict[item], a[item]
+    
